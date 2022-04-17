0eeadbf6c268a52319f07e8581073324eeb769c8
Kirill Panchencko <videokirills@gmail.com>
2022-04-18 02:08:38 +0300

    Changes in forms

--- my_site/people/forms.py
+++ my_site/people/forms.py
@@ -1,5 +1,4 @@
-from django.forms import ModelForm, Textarea, ModelChoiceField, TextInput, IntegerField, NumberInput, Select, Form, \
-    ChoiceField
+from django.forms import ModelForm, Textarea, ModelChoiceField, TextInput, IntegerField, NumberInput, Select, Form, ChoiceField
 from people.models import Person, Country
 
 
@@ -8,21 +7,15 @@ class CreatePersonForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['country'].empty_label = None
+        for field in self.fields.values():
+            field.widget.attrs = {"class": "form-control"}
 
     class Meta:
         model = Person
         fields = "__all__"
-        widgets = {
-            'first_name': TextInput(attrs={"class": "form-control"}),
-            'second_name': TextInput(attrs={"class": "form-control"}),
-            'biography': TextInput(attrs={"class": "form-control"}),
-            'age': NumberInput(attrs={"class": "form-control"}),
-            "country": Select(attrs={'class': "form-control"})
-        }
 
 
 class DeletePersonForm(Form):
     persons = [(p.id, f"{p.first_name} {p.second_name}") for p in Person.objects.all()]
     persons += [("", "Выберите человека")]
     person = ChoiceField(choices=persons, widget=Select(attrs={'class': "form-control"}))
-
